419203ac51db5701cfe667d5cb6fa10584921868
grassper <rishi.bharath27@gmail.com>
2022-04-28 20:11:58 +0530

    chore: packages updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
     'next',
